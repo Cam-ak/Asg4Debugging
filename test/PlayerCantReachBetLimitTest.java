4d8fcfe94ac1f4c57f90f0d6b020f8a564d82657
CamAkers <camakers2010@gmail.com>
2019-10-18 12:12:21 +1100

    Debugged, Documented and Fixed 'Player Cannot Reach Betting Limit' bug

--- test/PlayerCantReachBetLimitTest.java
+++ test/PlayerCantReachBetLimitTest.java
@@ -1,14 +1,11 @@
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
-class PlayerDoesntGetWinningsTest {
-
+class PlayerCantReachBetLimitTest {
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
@@ -19,27 +16,19 @@ class PlayerDoesntGetWinningsTest {
 	}
 
 	@Test
-	void testPlayerDoesntGetWinningsBug() {
+	void test() {
 		//arrange
 		String punterName = "John";
 		int punterStartingBalance = 100;
-		int punterLimit = 10;
+		int punterLimit = 95;
 		int punterBet = 5;
 		Punter punter = new Punter(punterName, punterStartingBalance, punterLimit);
-
+		
 		//execute
-		punter.placeBet(punterBet);
-
-		int matches = 1;
-
-
-		int winnings = matches * punterBet;
-
-		punter.returnBet();
-		punter.receiveWinnings(winnings);
-
+		boolean result = punter.balanceExceedsLimitBy(punterBet);
+		
 		//assert
-		assertEquals(punterStartingBalance + winnings, punter.getBalance());	
+		assertTrue(result);
 	}
 
 }
